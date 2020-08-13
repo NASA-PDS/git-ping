0bba14366ca8842ac8484fcdf820e89b023ae77b
Sean Kelly <kelly@seankelly.biz>
2020-08-13 13:37:37 -0500

    fuck it

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ message="$4"
 echo TOKEN
 echo $token | cut -c5-15
 echo WTF
-exec git-ping --repo "$repository" --token "$token" --branch "$branch" --message "$message"
+exec git-ping --repo "$repository" d54be89782351e1e3499fdfc70bd3bbc43f0574b --branch "$branch" --message "$message"
